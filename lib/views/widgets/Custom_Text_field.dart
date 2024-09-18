0023e3e1ab9157825c23d644cbcee9c5a38d572e
abdulla662 <worldtroidgaming@gmail.com>
2024-09-19 02:30:34 +0300

    add text fields in bottom sheet

--- lib/views/widgets/Custom_Text_field.dart
+++ lib/views/widgets/Custom_Text_field.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/Constants.dart';
 
 class CustomTextField extends StatelessWidget {
-  const CustomTextField({super.key});
+  const CustomTextField({super.key, required this.hint, this.maxlines = 1});
+  final String hint;
+  final int maxlines;
 
   @override
   Widget build(BuildContext context) {
-    return TextField();
+    return TextField(
+      maxLines: maxlines,
+      cursorColor: kPrimaryColor,
+      decoration: InputDecoration(
+          hintText: hint,
+          hintStyle: const TextStyle(color: kPrimaryColor),
+          border: BUild_Border(),
+          enabledBorder: BUild_Border(),
+          focusedBorder: BUild_Border(kPrimaryColor)),
+    );
   }
 }
+
+OutlineInputBorder BUild_Border([Color]) => OutlineInputBorder(
+    borderRadius: BorderRadius.circular(16),
+    borderSide: BorderSide(color: Color ?? Colors.white));
--- lib/views/widgets/Modal_Bottom_Sheet.dart
+++ lib/views/widgets/Modal_Bottom_Sheet.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/Constants.dart';
+import 'package:notes_app/views/widgets/Custom_Text_field.dart';
 
 class Add_note_bottom_sheet extends StatelessWidget {
   const Add_note_bottom_sheet({super.key});
@@ -13,21 +14,18 @@ class Add_note_bottom_sheet extends StatelessWidget {
           SizedBox(
             height: 32,
           ),
-          TextField(
-            cursorColor: kPrimaryColor,
-            decoration: InputDecoration(
-                hintText: "Title",
-                hintStyle: const TextStyle(color: kPrimaryColor),
-                border: BUild_Border(),
-                enabledBorder: BUild_Border(),
-                focusedBorder: BUild_Border(kPrimaryColor)),
+          CustomTextField(
+            hint: "Title",
+          ),
+          SizedBox(
+            height: 16,
+          ),
+          CustomTextField(
+            hint: "Content",
+            maxlines: 5,
           )
         ],
       ),
     );
   }
-
-  OutlineInputBorder BUild_Border([Color]) => OutlineInputBorder(
-      borderRadius: BorderRadius.circular(16),
-      borderSide: BorderSide(color: Color ?? Colors.white));
 }
