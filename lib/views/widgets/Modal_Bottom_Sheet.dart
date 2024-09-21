0fd3f68c7d276d0d605b35edc2fac7480a2c3cb5
abdulla662 <worldtroidgaming@gmail.com>
2024-09-21 22:37:36 +0300

    create cubit model

--- lib/views/widgets/Modal_Bottom_Sheet.dart
+++ lib/views/widgets/Modal_Bottom_Sheet.dart
@@ -10,27 +10,64 @@ class Add_note_bottom_sheet extends StatelessWidget {
     return const Padding(
       padding: EdgeInsets.symmetric(horizontal: 16),
       child: SingleChildScrollView(
-        child: Column(
-          children: [
-            SizedBox(
-              height: 32,
-            ),
-            CustomTextField(
-              hint: "Title",
-            ),
-            SizedBox(
-              height: 16,
-            ),
-            CustomTextField(
-              hint: "Content",
-              maxlines: 5,
-            ),
-            SizedBox(
-              height: 16,
-            ),
-            Custom_Buttom()
-          ],
-        ),
+        child: add_note_form(),
+      ),
+    );
+  }
+}
+
+class add_note_form extends StatefulWidget {
+  const add_note_form({
+    super.key,
+  });
+
+  @override
+  State<add_note_form> createState() => _add_note_formState();
+}
+
+class _add_note_formState extends State<add_note_form> {
+  final GlobalKey<FormState> formkey = GlobalKey();
+  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+  String? title, subtitle;
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: formkey,
+      autovalidateMode: autovalidateMode,
+      child: Column(
+        children: [
+          const SizedBox(
+            height: 32,
+          ),
+          CustomTextField(
+            onsaved: (value) {
+              title = value;
+            },
+            hint: "Title",
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          CustomTextField(
+            onsaved: (value) {
+              subtitle = value;
+            },
+            hint: "Content",
+            maxlines: 5,
+          ),
+          SizedBox(
+            height: 16,
+          ),
+          Custom_Buttom(
+            ontap: () {
+              if (formkey.currentState!.validate()) {
+                formkey.currentState!.save();
+              } else {
+                autovalidateMode = AutovalidateMode.always;
+              }
+            },
+          )
+        ],
       ),
     );
   }
