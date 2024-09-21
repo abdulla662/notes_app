8d22ca133c2e3ff463e795eeba3a3214d92b7241
abdulla662 <worldtroidgaming@gmail.com>
2024-09-21 22:38:05 +0300

    create add note cubit model

--- lib/views/widgets/Custom_button.dart
+++ lib/views/widgets/Custom_button.dart
@@ -2,20 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:notes_app/Constants.dart';
 
 class Custom_Buttom extends StatelessWidget {
-  const Custom_Buttom({super.key});
-
+  const Custom_Buttom({super.key, this.ontap});
+  final void Function()? ontap;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      height: 55,
-      decoration: BoxDecoration(
-          color: kPrimaryColor, borderRadius: BorderRadius.circular(16)),
-      child: const Center(
-        child: Text(
-          "Add",
-          style: TextStyle(
-              color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
+    return GestureDetector(
+      onTap: ontap,
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        height: 55,
+        decoration: BoxDecoration(
+            color: kPrimaryColor, borderRadius: BorderRadius.circular(16)),
+        child: const Center(
+          child: Text(
+            "Add",
+            style: TextStyle(
+                color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
+          ),
         ),
       ),
     );
